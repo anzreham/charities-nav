dd53d77234403c4a3e3a4693e6a1da0561310004
mohamed beltagy <mmk.mmk0@gmaul.com>
2021-01-23 22:48:30 +0200

    change access tooken time to be 60 min

--- charity_project/settings.py
+++ charity_project/settings.py
@@ -146,7 +146,7 @@ REST_AUTH_SERIALIZERS = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
